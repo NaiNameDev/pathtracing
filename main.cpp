8f5d4bf2a7a46304bc51733475e1d4e69f8e3473
NaiName <nonameguy1000@gmail.com>
2026-09-04 19:38:46 +0300

    viewport rectangle

--- main.cpp
+++ main.cpp
@@ -6,16 +6,52 @@
 #include <engine/signal.hpp>
 #include <engine/shader.hpp>
 
-Engine engine(1920, 1080, "make in babus");
+Engine engine(1280, 720, "sigma clouds");
+Shader main_shader;
+unsigned int VBO;
+unsigned int VAO;
 
 void on_ready() {
 	glfwSetInputMode(engine.main_window.glfw_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetWindowSize(engine.main_window.glfw_window, 1280, 720);
+
+	main_shader.init_shader_program();
+	main_shader.attach_shader("FRAGMENT", "shaders/f_tracer.glsl");
+	main_shader.attach_shader("VERTEX", "shaders/v_tracer.glsl");
+	main_shader.create_shader_program();
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	float screen_rect[18] ={-1.0f, -1.0f, 0.0f,
+							1.0f, -1.0f, 0.0f,
+							-1.0f, 1.0f, 0.0f,
+
+							1.0f, -1.0f, 0.0f,
+							-1.0f, 1.0f, 0.0f,
+							1.0f, 1.0f, 0.0f};
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), screen_rect, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 }
 void process(float delta) {
+	main_shader.execute();
+
+	main_shader.set_uniform("u_time", (float)glfwGetTime());
+
+	glBindVertexArray(VAO);	
+	glDrawArrays(GL_TRIANGLES, 0, 18);
+
 	engine.print_fps_info();
 }
 void on_quit() {
-	std::cout << "quit\n";
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 }
 
 int main() {
---
+++ shaders/f_tracer.glsl
@@ -0,0 +1,9 @@
+#version 460 core
+
+out vec4 out_color;
+
+uniform float u_time;
+
+void main() {
+	out_color = vec4(1.0);
+}
